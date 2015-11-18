aa828818ee0490a2337f70f5595c0bafbde30a3b
LinuxArchitects <LinuxArchitecs@LinuxArchitects.fr>
2015-11-18 19:45:23 +0100

    first commit

---
+++ syncittohome.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+FULL_BW=$1
+LOW_BW=$2
+SLEEP=$3
+
+HOME=1.2.3.4
+USER=MyHomeUser
+FROM_FOLDER=/cygdrive/c/HANDBRAKE-OUT/
+TO_FOLDER=/DATA/Movie/
+
+while true
+h=`date +%H`
+
+  # entre 08 et 23  = 200
+  # entre 23 et 08
+
+	if  [ $h -gt 00 ]  && [ $h -lt 07 ] ; then
+  		echo "full BW"
+  		bwlimit="$FULL_BW"
+	else
+  		echo "low BW"
+		bwlimit="$LOW_BW"
+	fi
+
+	do  rsync -P -avz --bwlimit=$bwlimit --append-verify -e "ssh -p 443" $FROM_FOLDER*.mp4 $USER@$HOME:$TO_FOLDER
+	echo "sleeping"
+	sleep $SLEEP
+	done
